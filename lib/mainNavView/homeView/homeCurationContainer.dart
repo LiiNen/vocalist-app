d8ff9f7d1784a93451d23de4010b5135d14e35b8
LiiNen <kjeonghoon065@gmail.com>
2021-11-14 17:12:22 +0900

    Add main suggestion box

--- lib/mainNavView/homeView/homeCurationContainer.dart
+++ lib/mainNavView/homeView/homeCurationContainer.dart
@@ -11,26 +11,81 @@ class _HomeCurationContainer extends State<HomeCurationContainer> {
   int typeIndex;
   _HomeCurationContainer(this.typeIndex);
 
+  var suggestionCurationList = ['', '', '', '', ''];
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurationSuggestion();
+  }
+
+  _getCurationSuggestion() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 14),
-      width: MediaQuery.of(context).size.width,
       child: Column(
         children: [
           Row(
             children: [
               Container(
-                margin: EdgeInsets.only(left: 22),
+                margin: EdgeInsets.only(left: 22, bottom: 10),
                 child: Text('${curationType[typeIndex]}큐레이션', style: textStyle(color: Color(0xff7c7c7c), weight: 700, size: 14.0)),
               ),
             ]
           ),
+          curationScrollView(),
           Container(
             margin: EdgeInsets.only(top: 40, left: 14, right: 14),
             child: lineDivider(context: context)
           )
         ],
+      )
+    );
+  }
+
+  curationScrollView() {
+    return Container(
+      height: 128,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: suggestionCurationList.length*2+1,
+        itemBuilder: (BuildContext context, int index) {
+          if(index==0) return SizedBox(width: 12);
+          else if(index%2 == 1) return curationContainer(((index-1)/2).floor());
+          else return SizedBox(width: 22);
+        }
+      )
+    );
+  }
+
+  curationContainer(int index) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {},
+      child: Container(
+        width: 117, height: 117,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(23)),
+          boxShadow: [BoxShadow(
+            color: Color(0x29000000),
+            offset: Offset(3, 3),
+            blurRadius: 10,
+            spreadRadius: 0
+          )],
+          color: Color(0xffe9e2f5)
+        ),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 13),
+          child: Column(
+            children: [
+
+            ]
+          )
+        )
       )
     );
   }
--- lib/mainNavView/homeView/homePlaylistContainer.dart
+++ lib/mainNavView/homeView/homePlaylistContainer.dart
@@ -6,21 +6,35 @@ class HomePlaylistContainer extends StatefulWidget {
   State<HomePlaylistContainer> createState() => _HomePlaylistContainer();
 }
 class _HomePlaylistContainer extends State<HomePlaylistContainer> {
+
+  var suggestionPlaylist = ['', '', ''];
+
+  @override
+  void initState() {
+    super.initState();
+    _getSuggestionPlaylist();
+  }
+
+  _getSuggestionPlaylist() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 14),
-      width: MediaQuery.of(context).size.width,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
               Container(
-                margin: EdgeInsets.only(left: 22),
+                margin: EdgeInsets.only(left: 22, bottom: 14),
                 child: Text('나만을 위한 AI 플레이리스트', style: textStyle(weight: 700, size: 14.0)),
               ),
             ]
           ),
+          playlistScrollView(),
           Container(
             margin: EdgeInsets.only(top: 40, left: 14, right: 14),
             child: lineDivider(context: context)
@@ -28,5 +42,61 @@ class _HomePlaylistContainer extends State<HomePlaylistContainer> {
         ],
       )
     );
+  }
+
+  playlistScrollView() {
+    return Container(
+      height: 226,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: suggestionPlaylist.length*2+1,
+        itemBuilder: (BuildContext context, int index) {
+          if(index==0) return SizedBox(width: 16);
+          else if(index%2 == 1) return playlistContainer(((index-1)/2).floor());
+          else return SizedBox(width: 23);
+        }
+      )
+    );
+  }
+
+  playlistContainer(int index) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {},
+      child: Container(
+        width: 219, height: 215,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(23)),
+          boxShadow: [BoxShadow(
+            color: Color(0x29000000),
+            offset: Offset(3, 3),
+            blurRadius: 10,
+            spreadRadius: 0
+          )],
+          color: Color(0xfff3f5f7)
+        ),
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 30, horizontal: 18),
+          child: Column(
+            children: [
+              subTitleBox(''),
+              mainTitleBox(''),
+              SizedBox(height: 18),
+              contentBox(''),
+            ]
+          )
+        )
+      )
+    );
+  }
+
+  subTitleBox(String title) {
+    return Text(title, style: textStyle(color: Color(0xff000000), weight: 500, size: 20.0));
+  }
+  mainTitleBox(String title) {
+    return Text(title, style: textStyle(color: Color(0xff000000), weight: 700, size: 25.0));
+  }
+  contentBox(String content) {
+    return Text(content, style: textStyle(color: Color(0xff000000), weight: 500, size: 12.0));
   }
 }
