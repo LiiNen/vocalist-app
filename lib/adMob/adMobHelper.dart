3b9d47fb110cf23eaa6b509daf2d902e5c422371
LiiNen <kjeonghoon065@gmail.com>
2022-03-13 22:47:56 +0900

    Update package name, ad key

--- lib/adMob/adMobHelper.dart
+++ lib/adMob/adMobHelper.dart
@@ -4,19 +4,11 @@ class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-2157411230431534/9542973323';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-2157411230431534/8377231313';
     }
     throw new UnsupportedError("Unsupported platform");
   }
 
-  static String get nativeAdUnitId {
-    if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/2247696110';
-    } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/3986624511';
-    }
-    throw new UnsupportedError("Unsupported platform");
-  }
 }
