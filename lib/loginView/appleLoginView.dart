f70732c1262995e266288fe95e14b7309e0fdc16
LiiNen <kjeonghoon065@gmail.com>
2024-07-27 17:56:00 +0900

    Remove apple login

--- lib/loginView/appleLoginView.dart
+++ lib/loginView/appleLoginView.dart
@@ -26,59 +26,61 @@ class _AppleLoginView extends State<AppleLoginView> {
   }
 
   void appleLogin() async {
-    final _firebaseAuth = FirebaseAuth.instance;
-    List<Scope> scopes = [Scope.email, Scope.fullName];
+    navigatorPush(context: context, widget: MainNavView(notice: true), replacement: true, all: true);
 
-    if(await AppleSignIn.isAvailable()) {
-      final AuthorizationResult result = await AppleSignIn.performRequests([
-        AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
-      ]);
+  //   final _firebaseAuth = FirebaseAuth.instance;
+  //   List<Scope> scopes = [Scope.email, Scope.fullName];
 
-      switch(result.status) {
-        case AuthorizationStatus.authorized:
-          final appleIdCredential = result.credential;
-          final oAuthProvider = OAuthProvider('apple.com');
-          final credential = oAuthProvider.credential(
-              idToken: String.fromCharCodes(appleIdCredential.identityToken),
-              accessToken: String.fromCharCodes(appleIdCredential.authorizationCode)
-          );
-          final authResult = await _firebaseAuth.signInWithCredential(credential);
-          final firebaseUser = authResult.user!;
-          if(firebaseUser.displayName == null) {
-            await firebaseUser.updateDisplayName('${appleIdCredential.fullName.familyName}${appleIdCredential.fullName.givenName}');
-          }
+  //   if(await AppleSignIn.isAvailable()) {
+  //     final AuthorizationResult result = await AppleSignIn.performRequests([
+  //       AppleIdRequest(requestedScopes: [Scope.email, Scope.fullName])
+  //     ]);
 
-          var loginResponse = await vloomLogin(firebaseUser.email!, 'apple');
-          if(loginResponse) {
-            final pref = await SharedPreferences.getInstance();
-            if(pref.getBool('isLogin')!) {
-              navigatorPush(context: context, widget: MainNavView(notice: true), replacement: true, all: true);
-            }
-          }
-          else {
-            var _confirm = await showSignUpConfirmDialog(context);
-            if(_confirm) {
-              var signUpResponse = await signupAction(email: firebaseUser.email!, name: firebaseUser.displayName!, type: 'apple');
-              if(signUpResponse == null) {
-                showToast('다시 시도해주세요');
-              }
-              else {
-                if (signUpResponse['id'] == 0) {
-                  showToast('이미 가입된 아이디입니다.');
-                }
-                showToast('회원가입완료! 다시 로그인해주세요');
-              }
-            }
-            else {
-              showToast('회원가입이 취소되었습니다.');
-            }
-          }
-          break;
-        case AuthorizationStatus.error:
-          break;
-        case AuthorizationStatus.cancelled:
-          break;
-      }
-    }
+  //     switch(result.status) {
+  //       case AuthorizationStatus.authorized:
+  //         final appleIdCredential = result.credential;
+  //         final oAuthProvider = OAuthProvider('apple.com');
+  //         final credential = oAuthProvider.credential(
+  //             idToken: String.fromCharCodes(appleIdCredential.identityToken),
+  //             accessToken: String.fromCharCodes(appleIdCredential.authorizationCode)
+  //         );
+  //         final authResult = await _firebaseAuth.signInWithCredential(credential);
+  //         final firebaseUser = authResult.user!;
+  //         if(firebaseUser.displayName == null) {
+  //           await firebaseUser.updateDisplayName('${appleIdCredential.fullName.familyName}${appleIdCredential.fullName.givenName}');
+  //         }
+
+  //         var loginResponse = await vloomLogin(firebaseUser.email!, 'apple');
+  //         if(loginResponse) {
+  //           final pref = await SharedPreferences.getInstance();
+  //           if(pref.getBool('isLogin')!) {
+  //             navigatorPush(context: context, widget: MainNavView(notice: true), replacement: true, all: true);
+  //           }
+  //         }
+  //         else {
+  //           var _confirm = await showSignUpConfirmDialog(context);
+  //           if(_confirm) {
+  //             var signUpResponse = await signupAction(email: firebaseUser.email!, name: firebaseUser.displayName!, type: 'apple');
+  //             if(signUpResponse == null) {
+  //               showToast('다시 시도해주세요');
+  //             }
+  //             else {
+  //               if (signUpResponse['id'] == 0) {
+  //                 showToast('이미 가입된 아이디입니다.');
+  //               }
+  //               showToast('회원가입완료! 다시 로그인해주세요');
+  //             }
+  //           }
+  //           else {
+  //             showToast('회원가입이 취소되었습니다.');
+  //           }
+  //         }
+  //         break;
+  //       case AuthorizationStatus.error:
+  //         break;
+  //       case AuthorizationStatus.cancelled:
+  //         break;
+  //     }
+  //   }
   }
 }
